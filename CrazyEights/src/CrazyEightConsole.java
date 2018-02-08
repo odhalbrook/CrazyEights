5993830b3502016695790957e7e3e71fa67644eb
owen.halbrook <owen.halbrook@MU-C1-12.mullen.pvt.k12.co.us>
2018-02-08 09:02:51 -0700

    added Deck class

--- CrazyEights/src/CrazyEightConsole.java
+++ CrazyEights/src/CrazyEightConsole.java
@@ -7,7 +7,6 @@ import javax.swing.*;
 
 public class CrazyEightConsole
 	{
-		static ArrayList<Card> deck = new ArrayList<Card>();
 		static ArrayList<Card> playerHand = new ArrayList<Card>();
 		static ArrayList<Card> consoleHand = new ArrayList<Card>();
 		static Card pile;
@@ -21,8 +20,6 @@ public class CrazyEightConsole
 		
 		public static void main(String[] args)
 			{
-				fillDeck();
-				shuffle();
 				dealCardPlayer();
 				dealCardConsole();
 				initialDeal();
@@ -30,77 +27,17 @@ public class CrazyEightConsole
 				gamePlay();
 			}
 
-		public static void fillDeck()
-			{
-			deck.add(new Card("king", "hearts", 13));
-			deck.add(new Card("king", "clubs", 13));
-			deck.add(new Card("king", "spades", 13));
-			deck.add(new Card("king", "diamonds", 13));
-			deck.add(new Card("queen", "hearts", 12));
-			deck.add(new Card("queen", "clubs", 12));
-			deck.add(new Card("queen", "spades", 12));
-			deck.add(new Card("queen", "diamonds", 12));
-			deck.add(new Card("jack", "hearts", 11));
-			deck.add(new Card("jack", "clubs", 11));
-			deck.add(new Card("jack", "spades", 11));
-			deck.add(new Card("jack", "diamonds", 11));
-			deck.add(new Card("ten", "hearts", 10));
-			deck.add(new Card("ten", "clubs", 10));
-			deck.add(new Card("ten", "spades", 10));
-			deck.add(new Card("ten", "diamonds", 10));
-			deck.add(new Card("nine", "hearts", 9));
-			deck.add(new Card("nine", "clubs", 9));
-			deck.add(new Card("nine", "spades", 9));
-			deck.add(new Card("nine", "diamonds", 9));
-			deck.add(new Card("eight", "hearts", 8));
-			deck.add(new Card("eight", "clubs", 8));
-			deck.add(new Card("eight", "spades", 8));
-			deck.add(new Card("eight", "diamonds", 8));
-			deck.add(new Card("seven", "hearts", 7));
-			deck.add(new Card("seven", "clubs", 7));
-			deck.add(new Card("seven", "spades", 7));
-			deck.add(new Card("seven", "diamonds", 7));
-			deck.add(new Card("six", "hearts", 6));
-			deck.add(new Card("six", "clubs", 6));
-			deck.add(new Card("six", "spades", 6));
-			deck.add(new Card("six", "diamonds", 6));
-			deck.add(new Card("five", "hearts", 5));
-			deck.add(new Card("five", "clubs", 5));
-			deck.add(new Card("five", "spades", 5));
-			deck.add(new Card("five", "diamonds", 5));
-			deck.add(new Card("four", "hearts", 4));
-			deck.add(new Card("four", "clubs", 4));
-			deck.add(new Card("four", "spades", 4));
-			deck.add(new Card("four", "diamonds", 4));
-			deck.add(new Card("three", "hearts", 3));
-			deck.add(new Card("three", "clubs", 3));
-			deck.add(new Card("three", "spades", 3));
-			deck.add(new Card("three", "diamonds", 3));
-			deck.add(new Card("two", "hearts", 2));
-			deck.add(new Card("two", "clubs", 2));
-			deck.add(new Card("two", "spades", 2));
-			deck.add(new Card("two", "diamonds", 2));
-			deck.add(new Card("ace", "hearts", 1));
-			deck.add(new Card("ace", "clubs", 1));
-			deck.add(new Card("ace", "spades", 1));
-			deck.add(new Card("ace", "diamonds", 1));
-			}
-		
-		public static void shuffle()
-			{
-			Collections.shuffle(deck);
-			}
 		
 		private static void dealCardPlayer()
 			{
-				playerHand.add(deck.get(0));
-				deck.remove(0);
+				playerHand.add(Deck.deck.get(0));
+				Deck.deck.remove(0);
 			}
 		
 		private static void dealCardConsole()
 			{
-				consoleHand.add(deck.get(0));
-				deck.remove(0);
+				consoleHand.add(Deck.deck.get(0));
+				Deck.deck.remove(0);
 				
 			}
 		
@@ -122,9 +59,9 @@ public class CrazyEightConsole
 				
 				if(answer1.equals("yes") || answer1.equals("Yes"))
 					{
-						for(int i = 0; i < deck.size();i++)
+						for(int i = 0; i < Deck.deck.size();i++)
 							{
-								System.out.println(deck.get(i).getRank() + " of " + deck.get(i).getSuit());
+								System.out.println(Deck.deck.get(i).getRank() + " of " + Deck.deck.get(i).getSuit());
 							}
 					}
 				else if(answer1.equals("no") || answer1.equals("No"))
